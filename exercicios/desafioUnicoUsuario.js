96c96ce50af6a0087723385a187432478dd491dc
Victor Ramos <victor.rmsp@gmail.com>
2021-01-27 20:36:12 -0300

    desafio unico usuario

---
+++ exercicios/desafioUnicoUsuario.js
@@ -0,0 +1,17 @@
+const env = require('../.env')
+const Telegraf = require('telegraf')
+const bot = new Telegraf(env.token)
+
+// responde apenas a um usuario especifico, com id presente no .env
+bot.start(ctx => {
+
+    let msgId = ctx.update.message.from.id
+
+    if (msgId == env.userId){
+        ctx.reply('Ao seu dispor mestre!')
+    } else {
+        ctx.reply('sinto muito, mas eu só falo com o meu mestre!')
+    }
+})
+
+bot.startPolling()
